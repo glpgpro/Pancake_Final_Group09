956cc1b7a6cff6951ff890c024c819a48709d3c4
Zydrax740 <shamsulammry740@gmail.com>
2024-12-07 23:34:20 +0800

    Created Playlists Page

---
+++ Pancake_Final/Configurations/Entities/Playlist_Seed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pancake_Final.Domain;
+
+namespace Pancake_Final.Configurations.Entities
+{
+    public class Playlist_Seed : IEntityTypeConfiguration<Playlist_Seed>
+    {
+
+        public void Configure(EntityTypeBuilder<Playlist_Seed> builder)
+        {
+            builder.HasData(
+
+               new Playlist
+               {
+
+               }
+
+
+           );
+        }
+    }
+}
